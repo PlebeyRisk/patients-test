dc6eab2e475d7e09ef6da2e039668a711bebfd41
PlebeyRisk <tuff546@gmail.com>
2020-01-19 23:41:02 +0300

    Updates

---
+++ precache-manifest.f749474a2945cf5152d7df14dd831e0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b363a8118f4f63b47f94a6061dfbb2ac",
+    "url": "/patients-test/index.html"
+  },
+  {
+    "revision": "3a46054e19e9aa87ab29",
+    "url": "/patients-test/static/css/main.392e985d.chunk.css"
+  },
+  {
+    "revision": "bf36373fa7391b257261",
+    "url": "/patients-test/static/js/2.996a3829.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/patients-test/static/js/2.996a3829.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a46054e19e9aa87ab29",
+    "url": "/patients-test/static/js/main.9669054c.chunk.js"
+  },
+  {
+    "revision": "d0accef20b1dd8d614bf",
+    "url": "/patients-test/static/js/runtime-main.95ea5e0d.js"
+  },
+  {
+    "revision": "db89d782b3a67f9d15b455ae23832676",
+    "url": "/patients-test/static/media/back.db89d782.svg"
+  }
+]);
